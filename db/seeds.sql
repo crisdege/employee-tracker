e61c28eead13c4de5933d773e8d14022e043698a
Cristina Dege <cris.dege0813@gmail.com>
2022-08-22 10:13:05 -0400

    schema and seeds started

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO department (name)
+VALUES 
+('Sales'),
+('Engineering'),
+('Finance'),
+('Legal');
+
+INSERT INTO role (title, department_id, salary)
+VALUES
+('Sales Lead', 1, 100000),
+('Salesperson', 1, 80000),
+('Lead Engineeer', 2, 150000),
+('Software Engineer', 2, 120000),
+('Account Manager', 3, 160000),
+('Accountant', 3, 125000),
+('Legal Team Lead', 4, 250000),
+('Lawher', 4, 190000);
